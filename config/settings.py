56ee46e883fe438a911a8d02edd5100c58c72d9a
VaibhavvvMehta <vaibhavmehta605@gmail.com>
2026-04-02 12:23:54 +0530

    models created

--- config/settings.py
+++ config/settings.py
@@ -28,9 +28,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)j$nb(y++&1&$%jh)m%0g6w-nx@m%wwpt!y2#%wkepihy&dbmh'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
 
-ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -120,6 +120,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = 'accounts.User'
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
